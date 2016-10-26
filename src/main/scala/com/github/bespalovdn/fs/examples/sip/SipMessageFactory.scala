afacc7161bd61203e10caa1e1fef47e631edf9c9
Dmitry Bespalov <bespalovdn@gmail.com>
2016-10-26 12:16:36 +0300

    sip-proxy: SipMessageFactory impl

--- src/main/scala/com/github/bespalovdn/fs/examples/sip/SipMessageFactory.scala
+++ src/main/scala/com/github/bespalovdn/fs/examples/sip/SipMessageFactory.scala
@@ -2,12 +2,23 @@ package com.github.bespalovdn.fs.examples.sip
 
 trait SipMessageFactory
 {
-    def ackRequest(response: SipResponse): SipRequest = ???
-    def byeRequest(): SipRequest = ???
-    def keepaliveRequest(): SipRequest = ???
-    def inviteRequest(sdp: String = null): SipRequest = ???
+    def ackRequest(response: SipResponse): SipRequest
+    def byeRequest(): SipRequest
+    def keepaliveRequest(): SipRequest
+    def inviteRequest(sdp: String = null): SipRequest
 
-    def okResponse(request: SipRequest): SipResponse = ???
-    def tryingResponse(request: SipRequest): SipResponse = ???
+    def okResponse(request: SipRequest): SipResponse
+    def tryingResponse(request: SipRequest): SipResponse
 }
 
+object SipMessageFactory
+{
+    def create(): SipMessageFactory = new SipMessageFactory {
+        override def ackRequest(response: SipResponse): SipRequest = ???
+        override def inviteRequest(sdp: String): SipRequest = ???
+        override def byeRequest(): SipRequest = ???
+        override def keepaliveRequest(): SipRequest = ???
+        override def okResponse(request: SipRequest): SipResponse = ???
+        override def tryingResponse(request: SipRequest): SipResponse = ???
+    }
+}
