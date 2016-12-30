99242cea48d7cb41c37179a4823c3f65316a86a4
Dmitry Bespalov <bespalovdn@gmail.com>
2016-12-30 15:10:21 +0300

    FunctionalStreamsTest intro

---
+++ src/test/scala/com/github/bespalovdn/fs/FunctionalStreamsTest.scala
@@ -0,0 +1,39 @@
+package com.github.bespalovdn.fs
+
+import java.util.concurrent.TimeoutException
+
+import org.scalatest._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.Failure
+
+class FunctionalStreamsTest extends FlatSpec
+    with Matchers
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with Inside
+{
+    implicit def executionContext: ExecutionContext = scala.concurrent.ExecutionContext.global
+
+    "The test" should "check if read with timeout works" in {
+        val endpoint = new Stream[Int, Int] {
+            override def read(timeout: Duration): Future[Int] = Promise[Int].future // never completes
+            override def write(elem: Int): Future[Unit] = Future.successful()
+        }
+
+        val consumer: ConstConsumer[Int, Int, Unit] = implicit stream => for {
+            _ <- stream.read(timeout = 10.millisecond)
+        } yield consume()
+
+        val result: Future[Unit] = endpoint <=> consumer
+        // check if it's not complete instantly:
+        result.value should be (None)
+        // wait for a while (more than timeout in consumer) and check if result completed with TimeoutException:
+        Thread.sleep(100)
+        inside (result.value) {
+            case Some(Failure(t: ActionFailedException)) =>
+                t.getCause shouldBe a [TimeoutException]
+        }
+    }
+}
