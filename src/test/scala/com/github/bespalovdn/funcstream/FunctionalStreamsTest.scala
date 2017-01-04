8a1f5e99ba24c7c6a67027f1d5ad69b1c8cc755f
Dmitry Bespalov <bespalovdn@gmail.com>
2017-01-04 13:40:15 +0300

    UT extension

--- src/test/scala/com/github/bespalovdn/funcstream/FunctionalStreamsTest.scala
+++ src/test/scala/com/github/bespalovdn/funcstream/FunctionalStreamsTest.scala
@@ -3,12 +3,15 @@ package com.github.bespalovdn.funcstream
 import java.util.concurrent.TimeoutException
 
 import com.github.bespalovdn.funcstream.impl.TimeoutSupport
+import org.junit.runner.RunWith
 import org.scalatest._
+import org.scalatest.junit.JUnitRunner
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.Failure
 
+@RunWith(classOf[JUnitRunner])
 class FunctionalStreamsTest extends FlatSpec
     with Matchers
     with BeforeAndAfterAll
@@ -35,6 +38,42 @@ class FunctionalStreamsTest extends FlatSpec
         Thread.sleep(100)
         inside (result.value) {
             case Some(Failure(t: Throwable)) => t shouldBe a [TimeoutException]
+        }
+    }
+
+    it should "check if stream forking works" in {
+        val endpoint = new FStream[Int, Int] {
+            private var _readCount: Int = 0
+            private val readValue: Iterator[Int] = Stream.from(1).iterator
+
+            override def read(timeout: Duration): Future[Int] = {
+                _readCount += 1
+                success(readValue.next())
+            }
+            override def write(elem: Int): Future[Unit] = success(())
+
+            def readCount: Int = _readCount
+        }
+
+        type Consumer = ConstConsumer[Int, Int, Unit]
+
+        val echoServer: Consumer = FConsumer { implicit stream => for {
+                a <- stream.read()
+                _ <- stream.write(a)
+            } yield consume ()
+        }
+
+        def check(fn: => Unit): Consumer = FConsumer{ implicit stream =>
+            fn
+            success(consume())
+        }
+
+        endpoint <=> {
+            check{
+                endpoint.readCount should be (0)
+            } >> echoServer >> check {
+                endpoint.readCount should be (1)
+            }
         }
     }
 }
