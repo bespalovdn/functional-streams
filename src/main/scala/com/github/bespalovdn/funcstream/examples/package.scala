ed9a673157ec942f5e619b8e02dc524bbb4bab80
Dmitry Bespalov <bespalovdn@gmail.com>
2017-01-08 13:22:01 +0300

    examples moved out

--- src/main/scala/com/github/bespalovdn/funcstream/examples/package.scala
+++
@@ -1,9 +0,0 @@
-package com.github.bespalovdn.funcstream
-
-import scala.concurrent.Future
-
-package object examples {
-    class ActionFailedException(cause: String) extends Exception(cause)
-
-    def fail[A](cause: String): Future[A] = Future.failed(new ActionFailedException(cause))
-}
--- src/main/scala/com/github/bespalovdn/funcstream/examples/sipproxy/SipProxy.scala
+++
@@ -1,5 +0,0 @@
-package com.github.bespalovdn.funcstream.examples.sipproxy
-
-object SipProxy extends App {
-    ???
-}
