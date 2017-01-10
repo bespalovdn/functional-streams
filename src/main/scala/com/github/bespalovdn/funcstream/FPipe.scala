a1462dd65e3e4592809d7c41f25aac27b0c7fe62
Dmitry Bespalov <bespalovdn@gmail.com>
2017-01-10 12:34:08 +0300

    FPipe factory method

--- src/main/scala/com/github/bespalovdn/funcstream/FPipe.scala
+++ src/main/scala/com/github/bespalovdn/funcstream/FPipe.scala
@@ -1,3 +1,10 @@
 package com.github.bespalovdn.funcstream
 
 trait FPipe[A, B, C, D] extends (FStream[A, B] => FStream[C, D])
+
+object FPipe
+{
+    def apply[A, B, C, D](fn: FStream[A, B] => FStream[C, D]): FPipe[A, B, C, D] = new FPipe[A, B, C, D] {
+        override def apply(stream: FStream[A, B]): FStream[C, D] = fn(stream)
+    }
+}
