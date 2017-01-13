6b51575edd92a389b634c438d0504086c7b16e9b
Dmitry Bespalov <bespalovdn@gmail.com>
2017-01-13 14:32:43 +0300

    FStream.DownStream improvements

--- src/main/scala/com/github/bespalovdn/funcstream/FStream.scala
+++ src/main/scala/com/github/bespalovdn/funcstream/FStream.scala
@@ -2,6 +2,7 @@ package com.github.bespalovdn.funcstream
 
 import java.io.Closeable
 import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicBoolean
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,10 +14,10 @@ trait FStream[A, B]
     def read(timeout: Duration = null): Future[A]
     def write(elem: B): Future[Unit]
 
-//    def <=> [C, D](pipe: FPipe[A, B, C, D]): FStream[C, D] = {
-//        val downStream = fork()
-//
-//    }
+    def <=> [C, D](pipe: FPipe[A, B, C, D]): FStream[C, D] = {
+        val downStream = fork()
+
+    }
 
     def <=> [C, D, X](c: FConsumer[A, B, C, D, X]): Future[X] = {
         val completion = Promise[Unit]
@@ -37,24 +38,35 @@ trait FStream[A, B]
     }
 
     private class DownStream extends FStream[A, B] with Closeable{
-
+        private val closed = new AtomicBoolean(false)
         private val readQueue = new ConcurrentLinkedQueue[Future[A]]()
 
         readQueues.synchronized{ readQueues += readQueue }
 
-        override def write(elem: B): Future[Unit] = upStream.synchronized{ upStream.write(elem) }
+        override def write(elem: B): Future[Unit] = {
+            if(closed.get())
+                fail(new StreamClosedException)
+            else
+                upStream.synchronized{ upStream.write(elem) }
+        }
+
         override def read(timeout: Duration): Future[A] = {
-            readQueue.poll() match {
-                case null =>
-                    val f = upStream.synchronized(upStream.read(timeout))
-                    readQueues.synchronized{ readQueues.foreach(_ offer f) }
-                    readQueue.poll()
-                case elem =>
-                    elem
+            if(closed.get()){
+                fail(new StreamClosedException)
+            } else {
+                readQueue.poll() match {
+                    case null =>
+                        val f = upStream.synchronized(upStream.read(timeout))
+                        readQueues.synchronized{ readQueues.foreach(_ offer f) }
+                        readQueue.poll()
+                    case elem =>
+                        elem
+                }
             }
         }
 
         override def close(): Unit = {
+            closed.set(true)
             readQueues.synchronized{ readQueues -= readQueue }
         }
     }
