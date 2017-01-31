81a28e7de4e340a0c34edab05595541f294f90b3
Dmitry Bespalov <bespalovdn@gmail.com>
2017-01-31 17:56:42 +0300

    update funcstream.success() method

--- src/main/scala/com/github/bespalovdn/funcstream/package.scala
+++ src/main/scala/com/github/bespalovdn/funcstream/package.scala
@@ -3,7 +3,7 @@ package com.github.bespalovdn
 import scala.concurrent.Future
 
 package object funcstream {
-    def success[A](value: A): Future[A] = Future.successful(value)
+    def success[A](value: => A): Future[A] = Future(value)
     def success(): Future[Unit] = success(())
 
     def fail[A](t: Throwable): Future[A] = Future.failed(t)
