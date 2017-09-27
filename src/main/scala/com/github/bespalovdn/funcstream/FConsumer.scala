ec178e9556da3a1f7da5feef6dbce71218f53563
Dmitry Bespalov <bespalovdn@gmail.com>
2017-09-27 22:58:43 +0300

    FConsumer: extra comments removed

--- src/main/scala/com/github/bespalovdn/funcstream/FConsumer.scala
+++ src/main/scala/com/github/bespalovdn/funcstream/FConsumer.scala
@@ -26,29 +26,3 @@ object FConsumer
     def empty[R, W]: FConsumer[R, W, Unit] = FConsumer{ stream => success() }
     def empty[R, W, A](a: => A)(implicit ec: ExecutionContext): FConsumer[R, W, A] = FConsumer{ stream => success(a) }
 }
-
-//trait FConsumer[-R, +W, A] {
-//    def apply[R1 <: R, W1 >: W](stream: FStream[R1, W1]): Future[A]
-//
-//    def >> [R1 <: R, W1 >: W, B](cB: => FConsumer[R1, W1, B])(implicit ec: ExecutionContext): FConsumer[R1, W1, B] = FConsumer {
-//        stream: FStream[R, W] => this.apply(stream) >> cB.apply(stream)
-//    }
-//    def >>= [R1 <: R, W1 >: W, B](cAB: A => FConsumer[R1, W1, B])(implicit ec: ExecutionContext): FConsumer[R1, W1, B] = FConsumer {
-//        stream: FStream[R, W] => this.apply(stream) >>= (x => cAB(x).apply(stream))
-//    }
-//    def map[B](fn: A => B)(implicit ec: ExecutionContext): FConsumer[R, W, B] = FConsumer{
-//        stream: FStream[R, W] => this.apply(stream).map(fn)
-//    }
-//    def flatMap[R1 <: R, W1 >: W, B](fn: A => FConsumer[R1, W1, B])(implicit ec: ExecutionContext): FConsumer[R1, W1, B] = this >>= fn
-//}
-//
-//object FConsumer
-//{
-//    def apply[R, W, A](fn: FStream[R, W] => Future[A]): FConsumer[R, W, A] = new FConsumer[R, W, A]{
-//        override def apply[R1 <: R, W1 >: W](stream: FStream[R1, W1]): Future[A] = fn(stream)
-//    }
-//
-//    def empty[R, W]: FConsumer[R, W, Unit] = FConsumer[R, W, Unit]{ stream => success() }
-//    def empty[R, W, A](a: => A)(implicit ec: ExecutionContext): FConsumer[R, W, A] = FConsumer[R, W, A]{ stream => success(a) }
-//}
-
